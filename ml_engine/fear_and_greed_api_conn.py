487e158e5c604bcedfaaa8d2fc3a133ac6301901
Datkap <k.dzida@datkap.com>
2022-01-25 22:56:06 +0100

    Appending each crypto df does not work. Need further investigation.

--- ml_engine/fear_and_greed_api_conn.py
+++ ml_engine/fear_and_greed_api_conn.py
@@ -1,8 +1,25 @@
 import requests as r
 import pandas as pd
 
-quotation_url = "https://api.alternative.me/v2/ticker/?limit=3&?convert=USD"
-quotation_conn = r.get(quotation_url).json()
+fiat_currency = 'USD'
+crypto_amount = 5
+
+quotation_url = f"https://api.alternative.me/v2/ticker/?limit={crypto_amount}&?convert={fiat_currency}"
+
+quotation_conn = r.get(quotation_url).json()['data']
+
+cryptocurrency_full_df = pd.DataFrame()
+
+for crypto_id in quotation_conn.keys():
+    crypto = quotation_conn[crypto_id]
+    params = quotation_conn[crypto_id]['quotes'][fiat_currency]
+    crypto.update(params)
+    del crypto['quotes']
+    crypto_df = pd.DataFrame.from_dict([crypto])
+    cryptocurrency_full_df.append(crypto_df, ignore_index=True)
+
+
+
 
 fear_and_greed_url = "https://api.alternative.me/fng/?limit=10&?format=csv"
 fear_and_greed_conn = r.get(fear_and_greed_url).text
